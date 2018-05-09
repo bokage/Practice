afc1b664bd484ee27c0eb09541d85fa543ffb566
bokage <37666121+bokage@users.noreply.github.com>
2018-05-09 10:52:37 -0700

    Update twoSum.java
    **Test**

--- twoSum.java
+++ twoSum.java
@@ -1,12 +1,12 @@
 public class twoSum{
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) { 
         for(int i = 0; i < nums.length; i++){
             for(int j = i; j < nums.length; j++){
                 if(i != j && nums[i] + nums[j] == target){
-                    return new int[] {i,j};
+                    return new int[] {i,j}; 
                 }
             }
         }
-        return new int[] {-1, -1};
+        return new int[] {-1, -1}; //returns this if there is no solution
     }
 }
