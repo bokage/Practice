7e95f59cc04875b97786999dbc7be93d7e60e9e5
bokage <37666121+bokage@users.noreply.github.com>
2018-05-09 14:55:06 -0700

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,10 @@
+public class palindrome{
+	public static String reverse(String input){
+		if(input.length()==1){
+			return input;
+		}
+		else{
+			return reverse(input.substring(1, input.length())) + input.substring(0,1);
+		}
+	}
+}
