f2190b807b21fafd12d950ce9c95eae23e0a1407
bokage <37666121+bokage@users.noreply.github.com>
2018-05-09 15:32:38 -0700

    Add files via upload

---
+++ reverseInt.java
@@ -0,0 +1,13 @@
+public class reverseInt{
+
+	public static int reverse(int n){
+		
+		if(n / 10 == 0){
+			return n;
+		}
+		else{
+			return (n % 10) * (int)(Math.pow(10, (int)(Math.log10(n)))) + reverse(n / 10);
+		}
+	}
+
+}
